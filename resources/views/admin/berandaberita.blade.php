84904f1c401a484b2434938c4d16f79fe90ea079
trisnasari <71631695+trisnasari@users.noreply.github.com>
2021-03-22 10:28:02 +0000

    Add files via upload

---
+++ resources/views/admin/berandaberita.blade.php
@@ -0,0 +1,38 @@
+<x-tamplate-layout>
+    <h2 class"font-semibold text-xl text-gray-800 leading-tight">
+    <h2 class="rounded-tl-3xl bg-gradient-to-r from-red-500 to-orange-800 p-4 shadow text-2xl text-white">
+        <h3 class="font-bold pl-2">{{$title}}</h3>
+</div>
+</h2>
+<div>
+    <div class""> 
+<div class="shadow overflow-hidden border-gray-200 sm:rounded-lg">
+    <table class="min-w-full divide-gray-200">
+         <thead class="bg-gray-50">
+            <tr>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">No</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nama</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Jabatan</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Alamat</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Aksi</th>
+            </tr>
+         </thead>
+         <tbody class="bg-white divide-y divide-gray-200">
+             <!--More items... -->
+            <tr>
+            <td class="px-6 py-4 whitespace-nowrap">1</td>
+            <td class="px-6 py-4 whitespace-nowrap">Ketut Suka</td>
+            <td class="px-6 py-4 whitespace-nowrap">Kepala Desa</td>
+            <td class="px-6 py-4 whitespace-nowrap"></td>
+            <td class="px-6 py-4 whitespace-nowrap">
+                <a href="#" class="text-red">Edit</a>
+                <a href="#" class="text-orange">Hapus</a>
+            </td>
+            </tr>>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</x-tamplate-layout>
